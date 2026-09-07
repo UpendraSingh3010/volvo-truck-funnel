56ca493fb3e308534687a5fd4d2041ba5518d963
UpendraSingh3010 <upendrasingh02005@gmail.com>
2026-09-07 18:48:10 +0530

    fix: convert images 3 and 4 from PNG to WebP (1.7MB -> 86KB each) to fix Vercel loading

--- src/components/sections/HowItWorksSection.jsx
+++ src/components/sections/HowItWorksSection.jsx
@@ -6,8 +6,8 @@ import { howItWorksData } from '../../data/landingContent';
 
 import img1 from '../../assets/images/1.avif';
 import img2 from '../../assets/images/2.avif';
-import img3 from '../../assets/images/3.png';
-import img4 from '../../assets/images/4.png';
+import img3 from '../../assets/images/3.webp';
+import img4 from '../../assets/images/4.webp';
 
 export default function HowItWorksSection() {
   const stepImages = [img1, img2, img3, img4];
